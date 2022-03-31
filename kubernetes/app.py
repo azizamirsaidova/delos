6f3d1d6191bd82855334dbf18fa848d86a1455eb
Aziza Mirsaidova <57295556+azizamirsaidova@users.noreply.github.com>
2022-03-31 18:35:37 -0500

    Add files via upload

--- kubernetes/app.py
+++ kubernetes/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+import time 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__,  )
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+app.route('/')
+
+def hello_world():
+    return jsonify({"Time to Call": time.time()})
+
+if __name__== "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
